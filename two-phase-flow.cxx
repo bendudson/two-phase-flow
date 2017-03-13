8a5f9cee84540568c574cd9a1fa840793de1d508
Ben Dudson <bd512@york.ac.uk>
2017-03-13 11:34:33 +0000

    Added CSF model for surface tension
    Uses a smoothed VOF function then finite difference
    to get the divergence of the normal vector. This curvature
    is then used in an Arakawa bracket expression in the
    vorticity equation.

--- two-phase-flow.cxx
+++ two-phase-flow.cxx
@@ -5,6 +5,8 @@
 #include <invert_laplace.hxx>
 
 const Field3D Div_VOF(const Field3D &n, const Field3D &f, BoutReal D=1.0);
+const Field3D Curvature(const Field3D &c, BoutReal eps=1e-5);
+const Field3D smoothXZ(const Field3D &f);
 
 /// Simulates incompressible flow of two fluids
 /// 
@@ -18,8 +20,13 @@ private:
   Field3D density; ///< Mass density. Calculated from vof at each iteration
   Field3D viscosity; ///< Kinematic viscosity. Calculated from vof
   
+  Field3D kappa; ///< Surface curvature
+  Field3D surf_force; ///< Surface forcing term in vorticity
+  Field3D vof_smooth; ///< Smoothed VOF used for curvature calculation
+
   BoutReal density0, density1; // Density of each fluid
   BoutReal viscosity0, viscosity1; // Kinematic viscosity of each fluid
+  BoutReal surface_tension; // sigma
 
   BoutReal gravity;
   
@@ -39,6 +46,7 @@ protected:
     OPTION(opt, density1, 0.1);
     OPTION(opt, viscosity0, 1.0);
     OPTION(opt, viscosity1, 0.1);
+    OPTION(opt, surface_tension, 0.0);
 
     OPTION(opt, gravity, 0.1);
     
@@ -50,6 +58,9 @@ protected:
     // Save the stream function at each output
     SAVE_REPEAT(psi);
     
+    // Save the curvature
+    SAVE_REPEAT3(kappa, surf_force, vof_smooth);
+
     // Create Laplacian inversion solver
     laplace = Laplacian::create(Options::getRoot()->getSection("laplace"));
     
@@ -94,6 +105,16 @@ protected:
       viscosity[i] = c*viscosity1 + (1.-c)*viscosity0;
     }
     
+    // Calculate curvature. Note we have to smooth the VOF function
+    // to reduce spurious curvatures
+
+    vof_smooth = vof;
+    for(int i=0;i<6;i++) {
+      vof_smooth = smoothXZ(vof_smooth);
+    }
+    kappa = Curvature(vof_smooth, 1e-9);
+    mesh->communicate(kappa);
+
     // Calculate stream function
     psi = laplace->solve(vorticity / density);
     mesh->communicate(psi); // Communicates guard cells
@@ -102,10 +123,13 @@ protected:
     ddt(vof) = -Div_VOF(vof, psi, vof_D);
 
     // vorticity equation
+    surf_force = surface_tension * bracket(kappa, vof, BRACKET_ARAKAWA);
+
     ddt(vorticity) =
       - bracket(psi, vorticity, BRACKET_ARAKAWA)
       - gravity * DDZ(density)
       + viscosity*Delp2(vorticity)
+      - surf_force
       ;
     return 0;
   }
@@ -115,6 +139,24 @@ protected:
 BoutReal minmod_vof(BoutReal a, BoutReal b, BoutReal c) {
   BoutReal sb = SIGN(b);
   return sb * BOUTMAX(0.0, BOUTMIN(sb*a, fabs(b), sb*c));
+}
+
+const Field3D smoothXZ(const Field3D &f) {
+  Field3D result;
+  
+  result = copy(f);
+  
+  for(const auto &i : result.region(RGN_NOBNDRY)) {
+    result[i] = 0.5*f[i] 
+      + (
+         f[i.xp()] + f[i.xm()]
+         + f[i.zp()] + f[i.zm()] 
+         + f[i.offset(1,0,1)] + f[i.offset(1,0,-1)]
+         + f[i.offset(-1,0,1)] + f[i.offset(-1,0,-1)]
+         ) / 16.;
+  }
+  mesh->communicate(result);
+  return result;
 }
 
 /*
@@ -150,25 +192,24 @@ const Field3D Div_VOF(const Field3D &n, const Field3D &f, BoutReal D) {
   for(int i=mesh->xstart;i<=mesh->xend+1;i++)
     for(int j=mesh->ystart;j<=mesh->yend;j++)
       for(int k=0;k<mesh->LocalNz;k++) {
-	int kp = (k+1) % (mesh->LocalNz);
-	int km = (k-1+mesh->LocalNz) % (mesh->LocalNz);
-	int kmm = (km-1+mesh->LocalNz) % (mesh->LocalNz);
+        int kp = (k+1) % (mesh->LocalNz);
+        int km = (k-1+mesh->LocalNz) % (mesh->LocalNz);
+        int kmm = (km-1+mesh->LocalNz) % (mesh->LocalNz);
 	
-	// 1) Interpolate stream function f onto corners fmp, fpp, fpm
+        // 1) Interpolate stream function f onto corners fmp, fpp, fpm
 	
-	BoutReal fmm = 0.25*(f(i,j,k) + f(i-1,j,k) + f(i,j,km) + f(i-1,j,km));
-	BoutReal fmp = 0.25*(f(i,j,k) + f(i,j,kp) + f(i-1,j,k) + f(i-1,j,kp)); // 2nd order accurate
-	BoutReal fpm = 0.25*(f(i,j,k) + f(i+1,j,k) + f(i,j,km) + f(i+1,j,km));
+        BoutReal fmm = 0.25*(f(i,j,k) + f(i-1,j,k) + f(i,j,km) + f(i-1,j,km));
+        BoutReal fmp = 0.25*(f(i,j,k) + f(i,j,kp) + f(i-1,j,k) + f(i-1,j,kp)); // 2nd order accurate
+        BoutReal fpm = 0.25*(f(i,j,k) + f(i+1,j,k) + f(i,j,km) + f(i+1,j,km));
 	
-	// 2) Calculate velocities on cell faces vD and vL
-	
-	BoutReal vD = J(i,j)*(fmm - fpm)/dx(i,j); // -J*df/dx
-	BoutReal vL = 0.5*(J(i,j)+J(i-1,j))*(fmp - fmm)/dz; // J*df/dz 
+        // 2) Calculate velocities on cell faces vD and vL
+        BoutReal vD = J(i,j)*(fmm - fpm)/dx(i,j); // -J*df/dx
+        BoutReal vL = 0.5*(J(i,j)+J(i-1,j))*(fmp - fmm)/dz; // J*df/dz 
 
-	// 3) Calculate n on the cell faces. The sign of the
-	//    velocity determines which side is used.
+        // 3) Calculate n on the cell faces. The sign of the
+        //    velocity determines which side is used.
 	
-	// X direction
+        // X direction
 	
         BoutReal c = n(i,j,k);
         BoutReal m = n(i-1,j,k);
@@ -177,7 +218,7 @@ const Field3D Div_VOF(const Field3D &n, const Field3D &f, BoutReal D) {
         
         // Left side
           
-	if ((i==mesh->xstart) && (mesh->firstX())) {
+        if ((i==mesh->xstart) && (mesh->firstX())) {
           // At left boundary in X
           
         } else {
@@ -208,8 +249,8 @@ const Field3D Div_VOF(const Field3D &n, const Field3D &f, BoutReal D) {
         p = n(i,j,kp);
         
         BoutReal flux;
-	if (vD < 0.0) {
-	  flux = vD * c;
+        if (vD < 0.0) {
+          flux = vD * c;
         }else {
           flux = vD * m;
         }
@@ -219,6 +260,76 @@ const Field3D Div_VOF(const Field3D &n, const Field3D &f, BoutReal D) {
         
         result(i,j,k)   -= flux;
         result(i,j,km)  += flux;
+      }
+  return result;
+}
+
+/// Calculate the curvature
+/// kappa = Div(n/|n|)
+///
+/// Calculation from: 
+/// http://elie.korea.ac.kr/~cfdkim/papers/surface_tension.pdf
+const Field3D Curvature(const Field3D &c, BoutReal eps) {
+  Field3D result(0.0);
+  
+  Coordinates *coord = mesh->coordinates();
+  Field2D J = coord->J;
+  Field2D dx = coord->dx;
+  BoutReal dz = coord->dz;
+  
+  for(int i=mesh->xstart;i<=mesh->xend+1;i++)
+    for(int j=mesh->ystart;j<=mesh->yend;j++)
+      for(int k=0;k<mesh->LocalNz;k++) {
+        // Calculate normal vector at vertex of each cell
+        // Need x and z components at (x,z) +/- (p/m) 1/2
+        // 
+        
+        int kp = (k+1) % (mesh->LocalNz);
+        int km = (k-1+mesh->LocalNz) % (mesh->LocalNz);
+
+        BoutReal nx_pp = (c(i+1,j,k) + c(i+1,j,kp) - c(i,j,k) - c(i,j,kp))/(2.*dx(i,j));
+        BoutReal nz_pp = (c(i,j,kp) + c(i+1,j,kp) - c(i,j,k) - c(i+1,j,k))/(2.*dz);
+        
+        BoutReal nx_mp = (c(i,j,k) + c(i,j,kp) - c(i-1,j,k) - c(i-1,j,kp))/(2.*dx(i-1,j));
+        BoutReal nz_mp = (c(i-1,j,kp) + c(i,j,kp) - c(i-1,j,k) - c(i,j,k))/(2.*dz);
+
+        BoutReal nx_pm = (c(i+1,j,km) + c(i+1,j,k) - c(i,j,km) - c(i,j,k))/(2.*dx(i,j));
+        BoutReal nz_pm = (c(i,j,k) + c(i+1,j,k) - c(i,j,km) - c(i+1,j,km))/(2.*dz);
+
+        BoutReal nx_mm = (c(i,j,km) + c(i,j,k) - c(i-1,j,km) - c(i-1,j,k))/(2.*dx(i,j));
+        BoutReal nz_mm = (c(i-1,j,k) + c(i,j,k) - c(i-1,j,km) - c(i,j,km))/(2.*dz);
+        
+        // Get magnitudes
+
+        BoutReal pp_mag = sqrt(SQ(nx_pp) + SQ(nz_pp));
+        BoutReal pm_mag = sqrt(SQ(nx_pm) + SQ(nz_pm));
+        BoutReal mp_mag = sqrt(SQ(nx_mp) + SQ(nz_mp));
+        BoutReal mm_mag = sqrt(SQ(nx_mm) + SQ(nz_mm));
+
+        // If any n vector is too small, set kappa to zero
+        if ( (pp_mag < eps) || (pm_mag < eps) || (mp_mag < eps) || (mm_mag < eps)) {
+            continue;
+          }
+        
+        // Normalise
+        
+        nx_pp /= pp_mag;
+        nz_pp /= pp_mag;
+        
+        nx_pm /= pm_mag;
+        nz_pm /= pm_mag;
+        
+        nx_mp /= mp_mag;
+        nz_mp /= mp_mag;
+
+        nx_mm /= mm_mag;
+        nz_mm /= mm_mag;
+
+        // Calculate divergence
+
+        result(i,j,k) = 0.5*(nx_pp + nx_pm - nx_mp - nx_mm)/dx(i,j)
+          + 0.5*(nz_mp + nz_pp - nz_mm - nz_pm)/dz;
+
       }
   return result;
 }
